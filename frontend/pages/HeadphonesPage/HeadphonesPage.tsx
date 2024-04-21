1d28736176f10e3d43b4084c4d1acb7dbb8295f3
Eric Lee <ericleecodes@gmail.com>
2024-04-21 00:35:09 -0700

    Finish Headphones page with GSAP animations

--- frontend/pages/HeadphonesPage/HeadphonesPage.tsx
+++ frontend/pages/HeadphonesPage/HeadphonesPage.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import { ShopContext } from "../../src/context/shop-context.tsx";
 import {
   ViewerApp,
@@ -6,6 +7,9 @@ import {
   AssetManagerPlugin,
   ScrollableCameraViewPlugin,
 } from "webgi";
+
+import processor from "./images/processor.png";
+
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useGSAP } from "@gsap/react";
@@ -16,41 +20,346 @@ function HeadphonesPage() {
   const canvasRef = useRef(null);
   const objectRef = useRef(null);
 
+  //GSAP Animation transitions
   useGSAP(() => {
     const tl = gsap.timeline();
 
-    tl.to(".section-two", {
-      xPercent: "10",
-      opacity: 1,
-      scrollTrigger: {
-        trigger: ".second",
-        start: "top center",
-        end: "bottom 99%",
-        scrub: true,
+    //second section
+    tl.fromTo(
+      ".section-two-right",
+      {
+        xPercent: "100",
+        opacity: 0,
       },
-    });
-    // second section
-    tl.to(".section-three", {
-      xPercent: "-110",
-      opacity: 1,
-      scrollTrigger: {
-        trigger: ".third",
-        start: "top center",
-        end: "bottom 99%",
-        scrub: true,
+      {
+        xPercent: "10",
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".second",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          markers: true,
+          snap: 1,
+        },
+      }
+    )
+      .fromTo(
+        ".section-two-right",
+        {
+          xPercent: "10",
+          opacity: 1,
+        },
+        {
+          xPercent: "10",
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: ".second",
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+          },
+        }
+      )
+      .fromTo(
+        ".section-two-left",
+        {
+          xPercent: "-100",
+          opacity: 0,
+        },
+        {
+          xPercent: "-280",
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".second",
+            start: "top center",
+            end: "bottom 100%",
+            scrub: true,
+            markers: true,
+            snap: 1,
+          },
+        }
+      )
+      .fromTo(
+        ".section-two-left",
+        {
+          xPercent: "-280",
+          opacity: 1,
+        },
+        {
+          xPercent: "-280",
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: ".second",
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+          },
+        }
+      );
+
+    //section-two LEFT
+
+    // third section
+    tl.fromTo(
+      ".section-three-top",
+      {
+        xPercent: "0",
+        opacity: 0,
       },
-    });
-    //third section
-    tl.to(".section-four", {
-      xPercent: "-10",
-      opacity: 1,
-      scrollTrigger: {
-        trigger: ".fourth",
-        start: "top center",
-        end: "bottom 99%",
-        scrub: true,
+      {
+        yPercent: "-150",
+        xPercent: "-200",
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".third",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          snap: 1,
+        },
+      }
+    );
+    tl.fromTo(
+      ".section-three-top",
+      {
+        yPercent: "-150",
+        xPercent: "-200",
+        opacity: 1,
       },
-    });
+      {
+        yPercent: "-150",
+        xPercent: "-200",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".third",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    );
+
+    tl.fromTo(
+      ".section-three-bottom",
+      {
+        xPercent: "0",
+        opacity: 0,
+      },
+      {
+        xPercent: "-150",
+        yPercent: "150",
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".third",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          snap: 1,
+        },
+      }
+    );
+    tl.fromTo(
+      ".section-three-bottom",
+      {
+        xPercent: "-150",
+        yPercent: "150",
+        opacity: 1,
+      },
+      {
+        xPercent: "-150",
+        yPercent: "150",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".third",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    );
+
+    //fourth section
+    tl.fromTo(
+      ".section-four-top",
+      {
+        xPercent: "0",
+        opacity: 0,
+      },
+      {
+        xPercent: "40",
+        yPercent: "-100",
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".fourth",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          snap: 1,
+        },
+      }
+    );
+    tl.fromTo(
+      ".section-four-top",
+      {
+        xPercent: "40",
+        yPercent: "-100",
+        opacity: 1,
+      },
+      {
+        xPercent: "40",
+        yPercent: "-100",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".fourth",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    );
+    tl.fromTo(
+      ".section-four-bottom",
+      {
+        xPercent: "0",
+        opacity: 0,
+      },
+      {
+        xPercent: "-380",
+        yPercent: "120",
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".fourth",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          snap: 1,
+        },
+      }
+    );
+    tl.fromTo(
+      ".section-four-bottom",
+      {
+        xPercent: "-380",
+        yPercent: "120",
+        opacity: 1,
+      },
+      {
+        xPercent: "-380",
+        yPercent: "120",
+        opacity: 0,
+        duration: 1,
+        ease: "power1.inOut",
+        scrollTrigger: {
+          trigger: ".fourth",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    );
+
+    //Fifth Section
+    tl.fromTo(
+      ".section-five-left",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".fifth",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          markers: true,
+          snap: 1,
+        },
+      }
+    );
+    tl.fromTo(
+      ".section-five-right",
+      {
+        opacity: 1,
+      },
+      {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+          trigger: ".fifth",
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    )
+      .fromTo(
+        ".section-five-right",
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          scrollTrigger: {
+            trigger: ".fifth",
+            start: "top center",
+            end: "bottom 100%",
+            scrub: true,
+            markers: true,
+            snap: 1,
+          },
+        }
+      )
+      .fromTo(
+        ".section-five-right",
+        {
+          opacity: 1,
+        },
+        {
+          opacity: 0,
+          duration: 1,
+          scrollTrigger: {
+            trigger: ".fifth",
+            start: "center center",
+            end: "bottom center",
+            scrub: true,
+          },
+        }
+      );
+
+    tl.fromTo(
+      ".section-six",
+      {
+        xPercent: "-100",
+        opacity: 0,
+      },
+      {
+        xPercent: "270",
+        yPercent: "-15",
+        opacity: 1,
+        scrollTrigger: {
+          trigger: ".sixth",
+          start: "top center",
+          end: "bottom 100%",
+          scrub: true,
+          markers: true,
+          snap: 1,
+        },
+      }
+    );
+    return () => {
+      tl.revert();
+    };
   });
 
   //Context
@@ -96,55 +405,108 @@ function HeadphonesPage() {
         viewer.dispose();
       }
     };
-  }, []);
+  });
 
   return (
     <>
       <div id="content">
-        <section className="section">
-          <h1>Sony WH-1000XM3</h1>
-        </section>
-        <section className="section second">
-          <div className="section-two">
-            <h2>Noise Cancelling</h2>
-            <p>LALALALALALALAL</p>
+        <section className="section grey">
+          <div className="section-one">
+            <h1 className="text-border">Sony WH-1000XM3</h1>
           </div>
+        </section>
+
+        <section className="section second">
+          <div className="section-two-left">
+            <img src={processor} alt="processor" />
+            <h4>Noise Cancelling Processor</h4>
+            <div>
+              Captures ambient noise and passes the data to the HD Noise
+              Cancelling Processor QN1
+            </div>
+          </div>
+          <div className="section-two-right">
+            <h2>Active Noise Cancelling</h2>
+            <p>
+              Boast powerful active noise cancellation technology, effectively
+              blocking out unwanted background noise for an immersive listening
+              experience.
+            </p>
+          </div>
+
           <div></div>
         </section>
 
-        <section className="section third">
-          <div className="section-three">
-            <h2>Soft Padding</h2>
-            <p>LALALALALALALAL</p>
+        <section className="section third grey">
+          <div className="section-three-top">
+            <h2 className="text-border">Luxuriously Soft Padding</h2>
+          </div>
+          <div className="section-three-bottom">
+            <h2 className="text-border">Comfort From Heaven</h2>
           </div>
           <div></div>
         </section>
 
         <section className="section fourth">
-          <div className="section-four">
-            <h2>Versatile Padding</h2>
-            <p>LALALALALALALAL</p>
+          <div className="section-four-top">
+            <h2>USB C</h2>
+            <p>
+              Versatile, lightning-fast, universally compatible, revolutionizing
+              connectivity across devices, embracing the future of technology.
+            </p>
+          </div>
+          <div className="section-four-bottom">
+            <h2>Hi-Fi LDAC Codec</h2>
+            <p>
+              LDAC delivers high-res audio wirelessly with unparalleled fidelity
+              and clarity.
+            </p>
           </div>
           <div></div>
         </section>
 
-        <section className="section fifth">
-          <div className="section-five">
-            <h2>Assistance On Demand</h2>
-            <p>LALALALALALALAL</p>
+        <section className="section fifth grey">
+          <div className="section-five-left">
+            <h2 className="title-color-black">Assistance On Demand</h2>
+            <p className="black-text">
+              Experience the convenience of instant assistance at your
+              fingertips with the WH-1000XM3 headphones, which seamlessly
+              integrate a voice assistant feature for on-demand help whenever
+              you need it.
+            </p>
+          </div>
+          <div className="section-five-right">
+            <h2 className="title-color-black">Versatile Touch Features</h2>
+            <p className="black-text">
+              The touch feature on the Sony WH-1000XM3 headphones offers
+              intuitive control at your fingertips, allowing you to effortlessly
+              manage your music playback, adjust volume, answer calls, and
+              activate your voice assistant—all with simple touch gestures.
+              Whether you're pausing your playlist with a tap, skipping tracks
+              with a swipe, or adjusting the volume with a slide, the
+              touch-sensitive controls provide a seamless and responsive
+              experience, putting you in complete command of your audio
+              environment without having to reach for your device.
+            </p>
           </div>
           <div></div>
         </section>
 
         <section className="section sixth">
           <div className="section-six">
+            <h1>Come Join Us</h1>
             <h2>Close The World Around You</h2>
-            <button
-              className="addToCartButton"
-              onClick={() => contextValue && contextValue!.addToCart(1)}
-            >
-              Add To Cart
-            </button>
+
+            <div className="buttons">
+              <button
+                onClick={() => contextValue && contextValue!.addToCart(1)}
+              >
+                Add To Cart
+              </button>
+              <Link to="/">
+                <button>Back to Home</button>
+              </Link>
+            </div>
           </div>
           <div></div>
         </section>
