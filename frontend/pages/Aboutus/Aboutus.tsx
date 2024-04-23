2b0a4f322b9ffe86c53e9c5598c473dedc3b1c4b
TapiocaQueen990 <daniel.phou@hotmail.com>
2024-04-23 17:39:18 -0400

    added text for content and css styling

--- frontend/pages/Aboutus/Aboutus.tsx
+++ frontend/pages/Aboutus/Aboutus.tsx
@@ -11,16 +11,28 @@ function Aboutus() {
         ourselves!
       </p>
       <section className="section-jake">
-        jake placeholder
+        Jake - The Visionary As the visionary behind Dimensional, Jake brings
+        boundless creativity and an unwavering commitment to innovation. With a
+        keen eye for detail and a passion for pushing boundaries, Jake leads our
+        team in crafting unique and impactful experiences for our users.
       </section>
 
       <section className="section-daniel">
-        DANIEL
-        </section>
+        Daniel - The Creative Force At the heart of our design process is
+        Daniel, the creative force driving our visual identity. With a flair for
+        design and a deep understanding of user experience, Daniel brings
+        concepts to life with style and sophistication. Their passion for
+        aesthetics and storytelling shines through in every aspect of our
+        website.
+      </section>
 
       <section className="section-eric">
-        ERIC
-        </section>
+        Eric - The Architect Meet Eric, the mastermind behind the architecture
+        and functionality of our platform. With a knack for turning ideas into
+        reality, Eric ensures that every feature is not only aesthetically
+        pleasing but also seamlessly functional. Their dedication to perfection
+        and precision is the cornerstone of our success.
+      </section>
     </div>
   );
 }
